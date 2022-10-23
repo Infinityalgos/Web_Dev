c1182ed4bcf2d804477efe9947cd39c5649ca858
Infinity Algorithms <115204983+Infinityalgos@users.noreply.github.com>
2022-10-23 12:52:15 +0500

    Add files via upload

--- Munching Food/Resources/Function.js
+++ Munching Food/Resources/Function.js
@@ -1,5 +1,21 @@
-function ShowSearchBar(){
 
-    document.getElementById("SearchBar").style.display = "block";
-    document.getElementById("BtnSearch").innerHTML = "Search";
+function ShowSearchBar() {
+    /* A FUNCTION TO TOGGLE HIDE/VIEW THE SEARCH BAR. */
+    // PERFORM SEARCH THEN:
+    let ScSize = window.matchMedia("(max-width: 1030px)")
+    if (ScSize.matches) {
+        var SearchBar = document.getElementById("SearchBar");
+
+
+        if (SearchBar.style.display === "none") {
+            SearchBar.style.display = "block";
+        }
+        else {
+            SearchBar.style.display = "none";
+        }
+    }
+    else {
+        SearchBar.style.display = "block";
+
+    }
 }
